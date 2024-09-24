cf6bb226f2e8ec803d146d93b7d7889b99f0e8c4
EbenDomey <domeyeben4@gmail.com>
2024-09-24 10:59:51 +0000

    config:: e2e testing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,23 +2,33 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { Sequelize } from 'sequelize-typescript';
+import TestAgent from 'supertest/lib/agent';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let sequelize: Sequelize;
+  let agent: TestAgent;
+  // let accessToken: string;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    sequelize = moduleFixture.get<Sequelize>(Sequelize);
+
     await app.init();
+    agent = request(app.getHttpServer());
+  });
+
+  afterAll(async () => {
+    await sequelize.close();
+    await app.close();
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+    return agent.get('/').expect(404);
   });
 });
