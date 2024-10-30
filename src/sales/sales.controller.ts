8c8a928e1dc95ca872618067efc8d8021bca3640
owbird <owbirdphorccuo@gmail.com>
2024-10-30 07:28:11 +0000

    feat (sales): Implement create sale api

--- src/sales/sales.controller.ts
+++ src/sales/sales.controller.ts
@@ -7,6 +7,8 @@ import {
   Param,
   Delete,
   Query,
+  HttpStatus,
+  Logger,
 } from '@nestjs/common';
 import { SalesService } from './sales.service';
 import {
@@ -17,19 +19,33 @@ import {
 } from './dto/';
 import { ApiTags } from '@nestjs/swagger';
 import { CustomApiResponse } from 'src/shared/docs/decorators';
+import { ApiSuccessResponseDto } from 'src/utils/responses/success.response';
+import { throwError } from 'src/utils/responses/error.response';
 
 @ApiTags('Sales')
 @Controller('sales')
 export class SalesController {
+  private logger = new Logger(SalesController.name);
+
   constructor(private readonly salesService: SalesService) {}
 
   @CustomApiResponse(['authorize', 'success'], {
-    type: CreateSaleDto,
+    type: GetSalesDto,
     message: 'Sales created successfully',
   })
   @Post()
-  create(@Body() createSaleDto: CreateSaleDto) {
-    return this.salesService.create(createSaleDto);
+  async create(@Body() createSaleDto: CreateSaleDto) {
+    try {
+      const response = await this.salesService.create(createSaleDto);
+
+      return new ApiSuccessResponseDto(
+        response,
+        HttpStatus.CREATED,
+        'sale created successfully',
+      );
+    } catch (error) {
+      throwError(this.logger, error);
+    }
   }
 
   @CustomApiResponse(['authorize', 'paginated'], {
--- src/sales/sales.service.ts
+++ src/sales/sales.service.ts
@@ -1,10 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { GetSalesPaginationDto, CreateSaleDto, UpdateSalesDto } from './dto/';
+import { InjectModel } from '@nestjs/sequelize';
+import { Sale } from './models/sales.models';
+import { DrugsService } from 'src/inventory/drugs/drugs.service';
 
 @Injectable()
 export class SalesService {
-  create(_: CreateSaleDto) {
-    return 'This action adds a new sale';
+  constructor(
+    @InjectModel(Sale)
+    private saleRepository: typeof Sale,
+
+    private drugService: DrugsService,
+  ) {}
+
+  async create(dto: CreateSaleDto) {
+    await this.drugService.findOne(dto.drugId);
+
+    dto.saleNumber = `S-${new Date().getTime()}`;
+
+    const sale = await this.saleRepository.create({
+      ...dto,
+    });
+
+    return sale;
   }
 
   fetchAll(_: GetSalesPaginationDto) {
