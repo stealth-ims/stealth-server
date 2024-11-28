69bf7f71d1cdbc9732e5c5eca1e427cdcf3f31f8
EbenDomey <domeyeben4@gmail.com>
2024-11-28 20:48:16 +0000

    seed script

--- config/config.js
+++ config/config.js
@@ -19,6 +19,20 @@ const configuration = {
     host: process.env.DB_HOST,
     dialect: 'postgres',
   },
+  staging: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: false,
+        rejectUnauthorized: false,
+      },
+    },
+  },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
